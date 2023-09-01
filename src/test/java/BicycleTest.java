5cbfce66e6fb4542ddf6822a953b676853026e51
pedrocpacheco <cpedropacheco@hotmail.com>
2023-09-01 18:26:06 -0300

    test: BicycleTest all working

--- src/test/java/BicycleTest.java
+++ src/test/java/BicycleTest.java
@@ -7,35 +7,43 @@ import main.java.br.com.bikenow.model.dao.user.CustomerDAO;
 import main.java.br.com.bikenow.model.entity.bicycle.Bicycle;
 import main.java.br.com.bikenow.model.entity.user.Customer;
 import main.java.br.com.bikenow.model.infra.db.DB;
+import main.java.br.com.bikenow.model.service.bicycle.BicycleService;
 
 public class BicycleTest {
   public static void main(String[] args) {
     
-    BicycleDAO bicycleDAO = new BicycleDAO(DB.getOracleConnection());
+    // ? Criando Bicycle Service
+    BicycleService bicycleService = new BicycleService(DB.getOracleConnection());
 
-    Customer owner = new CustomerDAO(DB.getOracleConnection()).findByCpf("11111111211");
-    Bicycle bicycle = new Bicycle(3, "10", "M", "B", 20.50, "2005", "D", owner);
+    // * CREATE - Criando e Salvando Biciclceta
+    Customer owner = new CustomerDAO(DB.getOracleConnection()).findByCpf("11111111111");
+    Bicycle bicycle = new Bicycle(5, "10", "M", "B", 20.50, "2005", "D", owner);
   
-    //bicycleDAO.insert(bicycle);
+    bicycleService.createBicycle(bicycle);
 
-    List<Bicycle> bicycles = bicycleDAO.list();
-    ////bicycles.forEach(b -> System.out.println(b.toString()));
+    // ? READ - Lendo todas as bicicletas
+    List<Bicycle> bicycles = bicycleService.findAll();
+    bicycles.forEach(b -> System.out.println(b.toString()));
 
-    //Bicycle bicycle2 = bicycleDAO.findById(1);
-    ///System.out.println(bicycle2.toString());
-    
-    //List<Bicycle> bicycles2 = bicycleDAO.findByCpfCustomer("11111111211");
-    //bicycles2.forEach(b2 -> System.out.println(b2.toString()));
+    // ? READ - Lendo bicicleta por bicycleId
+    Bicycle bicycle2 = bicycleService.findById(5);
+    System.out.println(bicycle2.toString());
 
+    // ? READ - Lendo bicicletas por customerCpf
+    List<Bicycle> bicyclesByCPF = bicycleService.findByCustomer(bicycle.getOwnerCpf());
+    bicyclesByCPF.forEach(bc -> System.out.println(bc.toString()));
+
+    // * UPDATE - Atualizando Bicicleta
     bicycle.setSerialNumber("1");
     bicycle.setModel("N");
     bicycle.setBrand("L");
     bicycle.setPrice(40.20);
     bicycle.setYear("2019");
     bicycle.setDescription("R");
-    //bicycleDAO.update(bicycle);
+    bicycleService.update(bicycle);
 
-    bicycleDAO.delete(bicycle);
+    // ! DELETE - Deletando Bicicleta
+    bicycleService.delete(bicycle);
 
   }
 }
