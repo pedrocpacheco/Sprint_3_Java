eb6a1ad5452847e93db99974d500c1d4e5f6ae17
pedrocpacheco <cpedropacheco@hotmail.com>
2023-09-01 18:14:30 -0300

    test: AnalystTest all working

--- src/test/java/AnalystTest.java
+++ src/test/java/AnalystTest.java
@@ -2,31 +2,35 @@ package test.java;
 
 import java.util.List;
 
-import main.java.br.com.bikenow.model.dao.user.AnalystDAO;
 import main.java.br.com.bikenow.model.entity.user.Analyst;
 import main.java.br.com.bikenow.model.infra.db.DB;
+import main.java.br.com.bikenow.model.service.user.AnalystService;
 
 public class AnalystTest {
   public static void main(String[] args) {
 
     // ? Criando CustomerDAO
-    AnalystDAO analystDAO = new AnalystDAO(DB.getOracleConnection());
+    AnalystService analystService = new AnalystService(DB.getOracleConnection());
 
     // * Inserindo Customer
-    Analyst analyst = new Analyst(99, "Analista99", "analista99@gmail.com", "9999");
-    analystDAO.insert(analyst);
+    Analyst analyst = new Analyst(1000, "Analista99", "analista99@gmail.com", "111132111111");
+    analystService.createAnalyst(analyst);
 
     // * Atualizando Customer
-    analyst.setName("Dante");
+    analyst.setName("OI");
     analyst.setEmail("dante@gmail.com");
-    analystDAO.update(analyst);
+    analystService.updateAnalyst(analyst);
 
     // ? Listando Customers
-    List<Analyst> analysts = analystDAO.list();
+    List<Analyst> analysts = analystService.findAll();
     analysts.forEach(a -> System.out.println(a.toString()));
 
+    // ? Listando Analyst por Rm
+    Analyst analyst2 = analystService.findByRm("111132111111");
+    System.out.println(analyst2.toString());
+
     // ! Deletando Customer
-    analystDAO.delete(analyst);
+    analystService.deleteAnalyst(analyst);
     
   }
 }
