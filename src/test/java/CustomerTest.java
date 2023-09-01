5ad09725d2a875942bf1bfa49b16f5463efef1ce
pedrocpacheco <cpedropacheco@hotmail.com>
2023-09-01 18:15:13 -0300

    test: CustomerTest all working

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -2,31 +2,35 @@ package test.java;
 
 import java.util.List;
 
-import main.java.br.com.bikenow.model.dao.user.CustomerDAO;
 import main.java.br.com.bikenow.model.entity.user.Customer;
 import main.java.br.com.bikenow.model.infra.db.DB;
+import main.java.br.com.bikenow.model.service.user.CustomerService;
 
 public class CustomerTest {
   public static void main(String[] args) {
     
     // ? Criando CustomerDAO
-    CustomerDAO customerDAO = new CustomerDAO(DB.getOracleConnection());
+    CustomerService customerService = new CustomerService(DB.getOracleConnection());
 
     // * Inserindo Customer
-    Customer customer = new Customer(900, "Pedro", "pedro@gmail.com", "11111111311");
-    customerDAO.insert(customer);
+    Customer customer = new Customer(89, "Pedro", "pedro@gmail.com", "11211111311");
+    customerService.createCustomer(customer);
 
     // * Atualizando Customer
     customer.setName("Dante");
     customer.setEmail("Dante@gmail.com");
-    //customerDAO.update(customer);
+    customerService.updateCustomer(customer);
 
-    // ? Listando Customers
-    List<Customer> customers = customerDAO.list();
+    // ? READ - Listando todos
+    List<Customer> customers = customerService.findAll();
     customers.forEach(c -> System.out.println(c.toString()));
 
+    // ? READ - Listando por CPF
+    Customer customerFound = customerService.findByCpf(customer.getCpf());
+    System.out.println(customerFound.toString());
+
     // ! Deletando Customer
-    //customerDAO.delete(customer);
+    customerService.deleteCustomer(customer);
     
   }
 }
